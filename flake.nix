bdafbe2b77ac3c3002ef73ef7b4db7cb761b50cd
Oliver <oliverbarcock@gmail.com>
2026-07-27 00:07:39 +0100

    Clean up flake.nix inputs

--- flake.nix
+++ flake.nix
@@ -3,20 +3,13 @@
 	description = "NixOS System Flake";
 
 	inputs = {
-		
-		# Other examples of nixpkgs.url:
-		#
-		# nixpkgs = {
-		# 	url = "github:NixOS/nixpkgs/release-26.05";
-		# };
-		#
-		# nixpkgs.url = "github:NixOS/nixpkgs/release-26.05";
-
 		nixpkgs.url = "nixpkgs/release-26.05";
 
-		home-manager.url = "github:nix-community/home-manager/release-26.05";
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
-	};
+		home-manager = {
+            url = "github:nix-community/home-manager/release-26.05";
+		    inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
 
 	outputs = { self, nixpkgs, home-manager, ... }:
 		let
